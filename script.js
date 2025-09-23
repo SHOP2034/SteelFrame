50716b0f4a96d04190d2cebe76faed0a5b18939d
SHOP2034 <SHOPPAGINAS@GMAIL.COM>
2025-09-23 15:10:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("¡Gracias por contactarnos! Te responderemos pronto.");
+  this.reset();
+});
+
